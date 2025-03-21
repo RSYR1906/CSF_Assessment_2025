7100684481a50f95a7292b097e765e5a9fac3be7
RSYR1906 <rsim9977@gmail.com>
2025-03-21 09:17:08 +0800

    first commit

---
+++ server/src/main/java/vttp/batch5/csf/assessment/server/controllers/RestaurantController.java
@@ -0,0 +1,23 @@
+package vttp.batch5.csf.assessment.server.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api")
+public class RestaurantController {
+
+  // TODO: Task 2.2
+  // You may change the method's signature
+  public ResponseEntity<String> getMenus() {
+    return ResponseEntity.ok("{}");
+  }
+
+  // TODO: Task 4
+  // Do not change the method's signature
+  public ResponseEntity<String> postFoodOrder(@RequestBody String payload) {
+    return ResponseEntity.ok("{}");
+  }
+}
